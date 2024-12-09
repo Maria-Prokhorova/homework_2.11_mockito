90d23d77a963ff60bc37945655df0462cec1ddee
Maria_Prokhorova <luchiksolnza@gmail.com>
2024-12-09 10:48:09 +0700

    Initial commit

---
+++ src/test/java/pro/sky/homework_2/_EmployeeBook/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.homework_2._EmployeeBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
